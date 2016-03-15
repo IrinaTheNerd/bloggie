8da1ab205e14aadde823f3a03b7b5adbf93dd3c3
Irina <peti1_12@uni.worc.ac.uk>
2016-03-15 14:19:28 +0000

    slider

--- index.php
+++ index.php
@@ -48,29 +48,40 @@ include_once('include/header.php');
 			<!-- Testimonials http://www.webchief.co.uk/blog/simple-jquery-slideshow/ -->
 			<!-- http://css-plus.com/2013/10/create-your-own-jquery-image-slider/ -->
 			<!-- Needs work-->
-			<section id="testimonials" class="slider-container">
+			<section id="testimonials">
 				<div class="first feat">
+					<div class="swiper-container middle">
+							<div class="swiper-wrapper">
+									<div class="swiper-slide" data-hash="slide1">Slide 1</div>
+									<div class="swiper-slide" data-hash="slide2">Slide 2</div>
+									<div class="swiper-slide" data-hash="slide3">Slide 3</div>
+							</div>
+							<!-- Add Pagination -->
+							<div class="swiper-pagination"></div>
+							<!-- Add Arrows -->
+							<div class="swiper-button-next"></div>
+							<div class="swiper-button-prev"></div>
+					</div>
 
-					    <div class="middle">
 					    	<!--<input type="radio" id="testimony1" name="slide" checked>
 					 		<label for="testimony1"></label>-->
-							<div id="testimonial1" class="slider transition">
+						<!--	<div id="testimonial1" class="slider transition">
 					    </div>
 					     <div id="testimonial2" class="slider transition">
 					    </div>
 					     <div id="testimonial3" class="transition slider">
 					    </div>
 					    <div class="slideLink">
-					      <ul> <!--can add links -->
+					      <ul> <!--can add links
 					        <a href="#testimonial1"><li  class="link"></li></a>
 					        <a href="#testimonial2"><li  class="link"></li></a>
 					        <a href="#testimonial3"><li  class="link"></li></a>
 					       </ul>
 					    </div>
 
-					    </div>
+						</div>
 
-					</ul>
+					</ul>-->
 				</div>
 			</section>
 
