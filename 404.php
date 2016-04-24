69dea87eae3a30e43a1c1cd57662c17ddf349e38
Irina <peti1_12@uni.worc.ac.uk>
2016-04-24 20:56:46 +0100

    404 error

---
+++ 404.php
@@ -0,0 +1,16 @@
+
+<?php
+//awesome page
+include_once('php/config.php');
+define("PAGENAME","404 Error... Go back!");
+include_once('include/header.php');
+?>
+<section id="error-page">
+<header>
+			<h1>Oops! We can't find the page you're looking for...</h1>
+			<h2> Try going back</h2>
+</header>
+</section>
+<?php
+include_once('include/footer.php');
+?>
