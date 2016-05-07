b8d5a4f6e9869ea96586a82bc1931023cbf6e064
IrinaTheNerd <peti1_12@uni.worc.ac.uk>
2016-05-07 18:47:42 +0100

    trumbowyg

--- js/editor.js
+++ js/editor.js
@@ -6,11 +6,7 @@
     ['viewHTML'], //order of buttons showing on the wysiwyg
     ['formatting'],
     'btnGrp-semantic'
-
   ]
-  })
-  .on('dblclick', function(){
-    $(this).trumbowyg();
   });
   //adding attribute for sending data to the db
   $('textarea.trumbowyg-textarea').attr('name','main_text');
